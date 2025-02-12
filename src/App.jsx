034a268ee72b394db7d05586f4ba4f8209192d35
Kunal Pandey <pandeykunal2084@gmail.com>
2025-02-12 21:18:46 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate } from "react-router-dom";
+  Navigate
+} from "react-router-dom";
 
 import Home from './pages/home/Home';
 import Order from "./pages/order/Order";
 import Cart from "./pages/cart/Cart";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
-import NoPage from "./pages/nopage/NoPage";
 import MyState from "./context/data/MyState";  // Corrected casing in import
 import Login from "./pages/registration/Login";
 import Productinfo from "./pages/productinfo/Productinfo";
@@ -20,36 +20,35 @@ import UpdateProduct from "./pages/admin/page/UpdateProduct";
 function App() {
   return (
     <MyState>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/order" element={
-          <ProtectedRoute>
-            <Order/>
-          </ProtectedRoute>
-        } />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/dashboard" element={
-          <AdminRoute>
-            <Dashboard/>
-          </AdminRoute>
-        } />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/productinfo/:id" element={<Productinfo/>} />
-        <Route path="/addproduct" element={
-          <AdminRoute>
-            <AddProduct/>
-          </AdminRoute>
-        } />
-        <Route path="/updateproduct" element={
-          <AdminRoute>
-            <UpdateProduct/>
-          </AdminRoute>
-        } />
-        <Route path="/*" element={<NoPage/>} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={
+            <ProtectedRoute>
+              <Order />
+            </ProtectedRoute>
+          } />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={
+            <AdminRoute>
+              <Dashboard />
+            </AdminRoute>
+          } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/productinfo/:id" element={<Productinfo />} />
+          <Route path="/addproduct" element={
+            <AdminRoute>
+              <AddProduct />
+            </AdminRoute>
+          } />
+          <Route path="/updateproduct" element={
+            <AdminRoute>
+              <UpdateProduct />
+            </AdminRoute>
+          } />
+        </Routes>
+      </Router>
     </MyState>
   )
 }
